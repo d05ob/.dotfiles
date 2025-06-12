661bdf8d4d4aa1983b12f8de6a422489da0aff15
d0sob <kitzeru@gmail.com>
2025-06-12 12:19:04 +0530

    [UNTESTED] config linker implementation update

--- configlinker.sh
+++ configlinker.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-
+set -e
 cd ~/.dotfiles
 
-# Linking .config files in here
-for dir in .config/dmenu .config/dwm .config/st .config/wallpapers .config/nvim; do
-	ln -sf "$PWD/$dir" ~/.config/$(basename "$dir")
+# Linking .config files
+config_dirs=(
+    .config/dmenu
+    .config/dwm
+    .config/st
+    .config/wallpapers
+    .config/nvim
+    .config/hypr
+    .config/wofi
+    .config/waybar
+    .config/kitty
+)
+
+for dir in "${config_dirs[@]}"; do
+    ln -sf "$PWD/$dir" "$HOME/.config/$(basename "$dir")"
 done
 
-#Linking config files in home directory here
-for file in .bashrc .bash_profile .xinitrc .tmux.conf; do
-    ln -sf "$PWD/$file" ~/"$file"
+# Linking home directory dotfiles
+home_files=(
+    .bashrc
+    .bash_profile
+    .xinitrc
+    .tmux.conf
+)
+
+for file in "${home_files[@]}"; do
+    ln -sf "$PWD/$file" "$HOME/$file"
 done
+
